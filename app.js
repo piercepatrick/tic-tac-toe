02799c8af1a8049215a58bfed693f480fce24c0d
Pierce <piercepatrick2001@gmail.com>
2021-03-30 21:10:07 -0500

    display gameboard

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
+
 const gameBoard = (() => {
-    let display = ['x','x','x','o','o','o','x','x','x'];
-    return {display};
-  })();
-  
-
-
-
-const player {
-
-  };
-  
-  
-
-  
- 
+    let board = new Array(9);
+    const display = (a) => {
+        let items = document.getElementsByTagName('td');
+        for (var i = 0; i <= items.length; i++) {
+            items[i].innerHTML = a[i];
+        }
+    }
+    return {board,display}
+})();
+gameBoard.display(['x','x','x','x','x','x','x','x','x'])
