0c6d6fc08db5269065533d1ff4af61a26727c5b8
Pierce <piercepatrick2001@gmail.com>
2021-04-24 21:38:56 -0500

    rewrite gameboard module

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
+// put gameBoard and displayController in modules
+// put players in factory
+
+
+// need to have listeners for every td
+// when a td is clicked, if its player x's turn then put an x in that position in the array and call gameboard.display. else put a O
+
+
+
 
 const gameBoard = (() => {
-    let board = new Array(9);
-    const display = (a) => {
-        let items = document.getElementsByTagName('td');
-        for (var i = 0; i <= items.length; i++) {
-            items[i].innerHTML = a[i];
+    const board = ['','','','','','','','','']
+
+    const setField = (index, sign) => {
+        if (index > board.length) return
+        board[index] = sign
+    }
+
+    const getField = (index, sign) => {
+        if (index > board.length) return
+        return board[index]
+    }
+
+    const reset = () => {
+        for (let i =0; i < board.length; i++) {
+            board[i] = ""
         }
     }
-    return {board,display}
+
+    return {setField, getField, reset}
 })();
-gameBoard.display(['x','x','x','x','x','x','x','x','x'])
+
+
+
+
+
